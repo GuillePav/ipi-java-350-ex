a80df4fd8ac1469466258bf57eb80a36e27e7f1a
GuillePav <guillemette.pavic@yahoo.fr>
2019-03-14 14:59:16 +0100

    Travis

---
+++ src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
@@ -0,0 +1,38 @@
+package com.ipiecoles.java.java350.model;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import java.time.LocalDate;
+
+public class EmployeTest {
+
+    @Test
+    public void testGetNombreAnneeAncienneteNow(){
+        //Given = Initialisation des données d'entrée
+        Employe employe = new Employe();
+        LocalDate dateEmbauche = LocalDate.now();
+        employe.setDateEmbauche(dateEmbauche);
+
+        //When = Exécution de la méthode à tester
+        Integer nbAnnee = employe.getNombreAnneeAnciennete();
+
+        //Then = Vérifications de ce qu'a fait la méthode
+        Assertions.assertThat(nbAnnee).isEqualTo(0);
+    }
+
+    @Test
+    public void testGetNombreAnneeAncienneteAfter(){
+        //Given
+        Employe employe = new Employe();
+        employe.setDateEmbauche(LocalDate.now());
+
+
+
+        //When
+        Integer nbAnnee = employe.getNombreAnneeAnciennete();
+
+        //Then
+    }
+
+
+}
